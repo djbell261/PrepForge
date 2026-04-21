b8c4c8da266ade7294c89f96bd9ae3f860394010
Derwin Bell <djbell261@gmail.com>
2026-04-21 11:08:52 -0400

    Spring Project Intitialized

---
+++ prepforge/src/main/java/com/derwin/prepforge/PrepforgeApplication.java
@@ -0,0 +1,13 @@
+package com.derwin.prepforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepforgeApplication.class, args);
+	}
+
+}
---
+++ prepforge/src/test/java/com/derwin/prepforge/PrepforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derwin.prepforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
